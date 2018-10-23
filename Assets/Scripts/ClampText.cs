e2f004f2f92490479a1bd79f9c899c7f51d92915
stasoz <truestasoz@gmail.com>
2018-10-23 22:09:36 +0300

    v.0.2 add text and target images

---
+++ Assets/Scripts/ClampText.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class ClampText : MonoBehaviour,ITrackableEventHandler{
+
+    public Text nameLabel;
+    private TrackableBehaviour mTrackableBehaviour;
+    public Vector3 vectorForUp;
+
+    private bool mShowText = false;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+    void Update()
+    {
+        FollowTextByTarget();
+        CheckOnEnable();
+    }
+    
+    public void FollowTextByTarget()
+    {
+        Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        nameLabel.transform.position = namePos+vectorForUp;
+    }
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,TrackableBehaviour.Status newStatus)
+    {
+        if(newStatus == TrackableBehaviour.Status.DETECTED ||
+           newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            mShowText = true;
+        }
+        else
+        {
+            mShowText = false;
+        }
+    }
+    public void CheckOnEnable()
+    {
+        if (mShowText) nameLabel.gameObject.SetActive(true);
+        else nameLabel.gameObject.SetActive(false);
+    }
+}
